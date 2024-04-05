188c9fcafca4a9600eaade180927195b7e7be93e
Anton Keller <ToniWebDev@mail.ru>
2024-04-05 14:38:52 +0300

    user menu styling in process

--- pages/user-menu.vue
+++ pages/user-menu.vue
@@ -2,21 +2,58 @@
   <v-card class="c-card">
     <v-layout class="c-layout" full-height>
 
-      <v-navigation-drawer floating permanent>
-        <v-list density="compact" nav bg-color="indigo" rounded>
+      <v-navigation-drawer floating permanent width="280">
+
+        <v-list density="compact" nav bg-color="indigo-lighten-5" rounded>
+
           <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg">
             Аккаунт
+            <template v-slot:append>
+              <v-btn
+                  icon="mdi-location-exit"
+                  size="small"
+                  variant="text"
+                  color="indigo-lighten-1"
+              />
+            </template>
           </v-list-item>
+
           <v-divider></v-divider>
-          <v-list-item prepend-icon="mdi-account-group-outline" value="users">
+
+          <v-list-item prepend-icon="mdi-account-group-outline" color="indigo-accent-4" value="users">
             Пользователи
+            <template v-slot:append>
+              <v-badge
+                  color="info"
+                  content="12"
+                  inline
+              ></v-badge>
+            </template>
           </v-list-item>
-          <v-list-item prepend-icon="mdi-file-chart-outline" value="tasks">
+
+          <v-list-item prepend-icon="mdi-format-list-text" color="indigo-accent-4" value="tasks">
+            Список задач
+          </v-list-item>
+
+          <v-list-item prepend-icon="mdi-file-chart-outline" color="indigo-accent-4" value="reports">
             Отчеты
           </v-list-item>
-          <v-list-item prepend-icon="mdi-view-module" value="reports">
+
+          <v-list-item prepend-icon="mdi-view-module" color="indigo-accent-4" value="modules">
             Модули
+            <template v-slot:append>
+              <v-badge
+                  color="secondary"
+                  content="12"
+                  inline
+              ></v-badge>
+            </template>
           </v-list-item>
+
+          <v-list-item prepend-icon="mdi-card-account-details-star-outline" color="indigo-accent-4" value="subscribe">
+            Управление подпиской
+          </v-list-item>
+
         </v-list>
       </v-navigation-drawer>
 
@@ -25,6 +62,7 @@
 
           <v-card-title>
             <v-text-field
+                color="indigo"
                 :loading="searchIsLoading || true"
                 append-inner-icon="mdi-magnify"
                 density="compact"
@@ -37,52 +75,85 @@
           </v-card-title>
 
           <v-card-subtitle>
-            <v-chip prepend-icon="mdi-checkbox-marked-circle" density="comfortable" size="small" variant="outlined"
-                    color="indigo">Chip 1
-            </v-chip>
-            <v-chip prepend-icon="mdi-checkbox-marked-circle" density="comfortable" size="small" variant="outlined"
-                    color="pink">Chip 2
-            </v-chip>
-            <v-chip prepend-icon="mdi-checkbox-marked-circle" density="comfortable" size="small" variant="outlined"
-                    color="red">Chip 3
-            </v-chip>
-            <v-chip prepend-icon="mdi-checkbox-marked-circle" density="comfortable" size="small" variant="outlined"
-                    color="blue">Chip 4
-            </v-chip>
+            <div class="d-flex ga-2">
+              <v-menu transition="slide-x-transition">
+
+                <template v-slot:activator="{ props }">
+                  <v-btn
+                      @click="sorted=!sorted"
+                      color="primary"
+                      variant="elevated"
+                      density="compact"
+                      rounded="0"
+                      v-bind="props"
+                  >сортировать</v-btn>
+                </template>
+
+                <v-list>
+                  <v-list-item v-for="(item, index) in menuItems" :key="index" :value="index">
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+
+              </v-menu>
+
+              <v-chip density="comfortable" size="small" variant="flat"
+                      label
+                      color="indigo">Chip 1
+              </v-chip>
+
+              <v-chip density="comfortable" size="small" variant="flat"
+                      label
+                      color="pink">Chip 2
+              </v-chip>
+
+              <v-chip density="comfortable" size="small" variant="flat" label
+                      color="red">Chip 3
+              </v-chip>
+
+              <v-chip append-icon="mdi-checkbox-marked-circle" density="comfortable" size="small" variant="flat"
+                      label
+                      color="blue">Chip 4
+              </v-chip>
+
+            </div>
           </v-card-subtitle>
 
           <v-card-text>
-            <v-list lines="two" max-height="400px">
+            <v-list max-height="600px" lines="two">
               <v-list-item
-                  v-for="file in users"
-                  :key="file.title"
-                  :subtitle="file.subtitle"
-                  :title="file.title"
+                  v-for="user in users"
+                  :key="user.title"
+                  :subtitle="user.subtitle"
               >
+
+                <template v-slot:title>
+                  {{user.title}}
+                  <v-badge :color="Math.random() > 0.5 ? 'error' : 'success'" dot style="width: 8px; height: 12px" />
+                </template>
+
                 <template v-slot:prepend>
-                  <v-avatar :color="file.color">
-                    <v-icon color="white">{{ file.icon }}</v-icon>
+                  <v-avatar :color="user.color">
+                    <v-icon color="white">{{ user.icon }}</v-icon>
                   </v-avatar>
+                </template>
+
+                <template v-slot:default>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, in, libero! Impedit nesciunt nihil provident.
                 </template>
 
                 <template v-slot:append>
                   <v-btn
                       color="grey-lighten-1"
-                      icon="mdi-information"
+                      icon="mdi-open-in-new"
                       variant="text"
-                  ></v-btn>
+                  />
                 </template>
               </v-list-item>
             </v-list>
           </v-card-text>
 
           <v-divider></v-divider>
-
-          <v-card-actions>
-            <v-btn variant="outlined" color="indigo" prepend-icon="mdi-plus-box-outline">
-              Новая
-            </v-btn>
-          </v-card-actions>
         </v-card>
 
       </v-main>
@@ -105,58 +176,104 @@ export default {
       tab: 'option-1',
       drawer: true,
       rail: true,
+      sorted: false,
 
       users: [
         {
-          // id: 1,
-          // login:
           color: 'blue',
-          icon: 'mdi-clipboard-text',
+          icon: 'mdi-account-network',
           subtitle: 'Jan 20, 2014',
           title: 'Vacation itinerary',
         },
         {
           color: 'amber',
-          icon: 'mdi-gesture-tap-button',
+          icon: 'mdi-account-network',
           subtitle: 'Jan 10, 2014',
           title: 'Kitchen remodel',
         },
         {
           color: 'blue',
-          icon: 'mdi-clipboard-text',
+          icon: 'mdi-account-network',
           subtitle: 'Jan 20, 2014',
           title: 'Vacation itinerary',
         },
         {
-          color: 'amber',
-          icon: 'mdi-gesture-tap-button',
+          color: 'red',
+          icon: 'mdi-account-network',
           subtitle: 'Jan 10, 2014',
           title: 'Kitchen remodel',
         },
         {
           color: 'blue',
-          icon: 'mdi-clipboard-text',
+          icon: 'mdi-account-network',
           subtitle: 'Jan 20, 2014',
           title: 'Vacation itinerary',
         },
         {
           color: 'amber',
-          icon: 'mdi-gesture-tap-button',
+          icon: 'mdi-account-network',
+          subtitle: 'Jan 10, 2014',
+          title: 'Kitchen remodel',
+        },
+        {
+          color: 'red',
+          icon: 'mdi-account-network',
+          subtitle: 'Jan 20, 2014',
+          title: 'Vacation itinerary',
+        },
+        {
+          color: 'amber',
+          icon: 'mdi-account-network',
+          subtitle: 'Jan 10, 2014',
+          title: 'Kitchen remodel',
+        },
+        {
+          color: 'amber',
+          icon: 'mdi-account-network',
           subtitle: 'Jan 10, 2014',
           title: 'Kitchen remodel',
         },
         {
           color: 'blue',
-          icon: 'mdi-clipboard-text',
+          icon: 'mdi-account-network',
+          subtitle: 'Jan 20, 2014',
+          title: 'Vacation itinerary',
+        },
+        {
+          color: 'red',
+          icon: 'mdi-account-network',
+          subtitle: 'Jan 10, 2014',
+          title: 'Kitchen remodel',
+        },
+        {
+          color: 'blue',
+          icon: 'mdi-account-network',
           subtitle: 'Jan 20, 2014',
           title: 'Vacation itinerary',
         },
         {
           color: 'amber',
-          icon: 'mdi-gesture-tap-button',
+          icon: 'mdi-account-network',
           subtitle: 'Jan 10, 2014',
           title: 'Kitchen remodel',
         },
+        {
+          color: 'red',
+          icon: 'mdi-account-network',
+          subtitle: 'Jan 20, 2014',
+          title: 'Vacation itinerary',
+        },
+        {
+          color: 'red',
+          icon: 'mdi-account-network',
+          subtitle: 'Jan 10, 2014',
+          title: 'Kitchen remodel',
+        },
+      ],
+
+      menuItems: [
+        {title: 'По убыванию'},
+        {title: 'По возрастанию'},
       ],
 
       folders: [
