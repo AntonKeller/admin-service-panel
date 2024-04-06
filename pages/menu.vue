d93f9211cd69c3bef64d8beabf04b8537a6a55c6
Anton Keller <ToniWebDev@mail.ru>
2024-04-06 18:29:59 +0300

    user-menu rename

---
+++ pages/menu.vue
@@ -0,0 +1,18 @@
+<template>
+  <nuxt-layout name="menu">
+    <nuxt-page />
+  </nuxt-layout>
+</template>
+
+<script>
+export default {
+
+  name: 'MenuPage',
+
+  // beforeCreate: () => {
+  //   const router = useRouter()
+  //   router.back()
+  // },
+}
+
+</script>
