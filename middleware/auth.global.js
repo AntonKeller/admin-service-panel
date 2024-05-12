bdedabb6179b6d001e752888f6ffa40555937d5c
Anton Keller <ToniWebDev@mail.ru>
2024-05-12 20:24:43 +0300

    route's guard fix create store

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,22 +1,9 @@
-// import {accessTest} from "@/utils/methods/requests";
-//
-//
 export default defineNuxtRouteMiddleware(async (to, from) => {
+    // console.log('route:', /manager-menu/ig.test(to?.fullPath));
+    // console.log('route store:', user?.value);
+    // console.log('user?.value:', user?.value)
+    // console.log('status permit:', /manager-menu/ig.test(to?.fullPath) && user?.value)
+    // console.log('path:', to?.fullPath)
 
-    return;
-
-//     let tokenBlock = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('token')) : null;
-//     let accessToken = tokenBlock?.accessToken;
-//     let status = await accessTest(accessToken).catch(err => err?.response?.status);
-//
-//     return status == 401 ? abortNavigation() : null;
-//
-//
-//     // if (status == 401) {
-//     //     console.log("Не авторизован", status)
-//     //     return abortNavigation();
-//     // }
-//
-//     // if (status === 401) return abortNavigation();
-//     // return abortNavigation();
+    return /manager-menu/ig.test(to?.fullPath) && !useState('user')?.value  ? abortNavigation() : undefined;
 })
