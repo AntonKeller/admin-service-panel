67e2382d290b48acbbfffc6e750e5cec95d0264e
Anton Keller <ToniWebDev@mail.ru>
2024-05-14 14:14:10 +0300

    change field

--- utils/types/TAssignmentBlock.ts
+++ utils/types/TAssignmentBlock.ts
@@ -1,5 +1,8 @@
+import type {TAssignment} from "@/utils/types/TAssignment";
+
 export default interface TAssignmentBlock {
     _id?: string,
     plegeAgreement: string, // Номер договора залогодателя
     plegerName: string, // Наименование залогодателя
+    assignment: TAssignment,
 }
