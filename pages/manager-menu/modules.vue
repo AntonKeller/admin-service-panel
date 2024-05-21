abbb4b3207506f4e2c3bd56a414c575a02af5683
Anton Keller <ToniWebDev@mail.ru>
2024-05-21 17:44:35 +0300

    transport data string in process...

--- pages/manager-menu/modules.vue
+++ pages/manager-menu/modules.vue
@@ -43,55 +43,67 @@ export default {
 
     async getFile() {
 
-      let url = URL.createObjectURL(this.files[0]);
-      let link = document.createElement('a');
-      link.href = url;
-      link.download = "file.jpg";
-      document.body.append(link);
-      link.click();
+      // let url = URL.createObjectURL(this.files[0]);
+      // let link = document.createElement('a');
+      // link.href = url;
+      // link.download = "file.jpg";
+      // document.body.append(link);
+      // link.click();
+      let _id = "535e30b7-e771-4b02-824b-2d3fc9035778\n";
+      let responseData = await axios.get('http://192.168.1.30/download/' + _id).then(r => r?.data)
+      console.log('responseData', responseData)
 
-      // let responseData = await axios.get('http://192.168.1.30/photos').then(e => e?.data);
-
-
-      // console.log(Buffer.from(responseData[0].buffer))
-
-      //
-      // let blob = new Blob([responseData[0].buffer], {type: 'image/png'})
-      // console.log('blob', blob)
-      //
-      // // let blob = new Blob(["hello"]);
-      // let url = URL.createObjectURL(blob);
+      // new File()
+      // let blb = new Blob([responseData[0]?.files]);
+      // let url = URL.createObjectURL(responseData[0]?.files);
       // let link = document.createElement('a')
       // link.href = url;
-      // link.download = "file.png"
+      // link.download = "file.jpg"
       // document.body.append(link)
       // link.click();
     },
 
-    sendFile() {
+    async sendFile() {
 
-      let file = this.files?.shift()
-      let formData = new FormData();
-      formData.append('file', file);
+      let txt_buffer = await this.files[0].text();
+      console.log('array_buffer', txt_buffer)
+      axios.post(serverURL + '/photos/add', txt_buffer)
+          .then(resp => console.log('Файл добавлен успешно', resp))
+          .catch(err => console.log('Ошибка добавления файла', err))
 
-      console.log('file', file)
-      console.log('formData', formData)
 
-      // const formData = new FormData();
-      // formData.append("files", this.files)
+      // let blb = new Blob([binBuff])
+      // let blb_txt = await blb.text();
+      // // let blb2 = new Blob([blb_txt])
+      // let enc = new TextEncoder();
 
-      axios.post(serverURL + '/photos/add',
-          formData,
-          {
-            headers: {
-              'Content-Type': 'multipart/form-data'
-            }
-          }
-      ).then(response => {
-        console.log('response', response)
-      }).catch(err => {
-        console.log('err', err)
-      })
+      // console.log('binBuff', enc.encode(blb_txt))
+      // console.log('bytes', binBuff)
+      // let url = URL.createObjectURL(blb);
+      // let link = document.createElement('a')
+      // link.href = url;
+      // link.download = "file.jpg"
+      // document.body.append(link)
+      // link.click();
+
+      // let formData = new FormData();
+      // formData.append('image', this.files[0]);
+      //
+      // console.log('_file:', this.files[0])
+      // console.log('_formData:', formData)
+      //
+      // axios.post(serverURL + '/photos/add',
+      //     formData,
+      //     {
+      //       headers: {
+      //         'Content-Type': 'multipart/form-data'
+      //       }
+      //     }
+      // ).then(response => {
+      //   console.log('response', response)
+      // }).catch(err => {
+      //   console.log('err', err)
+      // })
 
     }
   }
