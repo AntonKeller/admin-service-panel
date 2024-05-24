bf08415fba16c1b7536c97773b8c4f87f983a32b
Anton Keller <ToniWebDev@mail.ru>
2024-05-24 20:48:13 +0300

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -62,7 +62,7 @@ export default defineNuxtConfig({
     },
 
     css: [
-        '/assets/css/main.css'
+        '/assets/css/main.scss'
     ],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
