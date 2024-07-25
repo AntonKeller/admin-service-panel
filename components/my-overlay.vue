9e8dbc8eb872bef644bfcc40bcd9c2cef54d599f
Anton Keller <ToniWebDev@mail.ru>
2024-07-25 22:06:15 +0300

    created

---
+++ components/my-overlay.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-overlay
+      v-bind="$attrs"
+      class="d-flex justify-center align-center"
+      transition="scroll-y-transition"
+  >
+    <slot/>
+  </v-overlay>
+</template>
+
+<script>
+export default {
+  name: "my-overlay"
+}
+</script>
---
+++ components/my-search-bar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="d-flex ga-2">
+    <v-btn
+        rounded="lg"
+        color="blue-grey-darken-2"
+        variant="elevated"
+        size="small"
+        icon="mdi-plus"
+        @click="$emit('btn:click')"
+    />
+    <v-text-field
+        v-bind="$attrs"
+        color="blue-grey-darken-2"
+        prepend-inner-icon="mdi-magnify"
+        density="compact"
+        label="Поиск по задачам"
+        variant="outlined"
+        hide-details
+        single-line
+        clearable
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  name: "my-search-bar",
+}
+</script>
