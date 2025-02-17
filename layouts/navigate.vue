8e776ecf05a8f174c1b0f782faeb68636250153e
Anton Keller <ToniWebDev@mail.ru>
2025-02-17 22:26:06 +0300

    fix nav bar

--- layouts/navigate.vue
+++ layouts/navigate.vue
@@ -1,154 +1,150 @@
 <template>
-  <v-card height="100vh">
-    <v-layout full-height>
-
-      <v-app-bar elevation="0">
-
-
-        <v-card
-            class="bg-blue-darken-2"
-            variant="text"
-            elevation="1"
-            style="min-width: 100%;"
-        >
-          <v-card-item>
-            <div class="d-flex justify-start align-center ga-4">
-
-              <v-list-item
-                  prepend-icon="mdi-account-cog-outline"
-                  variant="tonal"
-                  slim
-                  nav
-                  :title="profile.fullName"
-                  :subtitle="profile.email"
-                  @click="navigateToProfile"
-              >
-                <v-tooltip activator="parent" location="bottom">
-                  Настройки профиля
-                </v-tooltip>
-              </v-list-item>
-
-              <v-list-item
-                  prepend-icon="mdi-bell"
-                  title="Оповещения"
-                  variant="tonal"
-                  slim
-                  nav
-                  disabled
-                  @click=""
-              >
-                <v-tooltip activator="parent" location="bottom">
-                  Проверить оповещения
-                </v-tooltip>
-              </v-list-item>
-
-              <v-list-item
-                  prepend-icon="mdi-finance"
-                  title="Статистика"
-                  variant="tonal"
-                  slim
-                  nav
-                  disabled
-                  @click=""
-              >
-                <v-tooltip activator="parent" location="bottom">
-                  Просмотр статистики
-                </v-tooltip>
-              </v-list-item>
-
-              <v-spacer/>
-
-              <v-label class="text-caption">
-                Текущий баланс аккаунта 1 000 объектов
-              </v-label>
-
-              <v-list-item
-                  prepend-icon=""
-                  variant="outlined"
-                  class="white"
-                  density="compact"
-                  title="Пополнить баланс"
-                  slim
-                  disabled
-                  nav
-                  @click=""
-              >
-              </v-list-item>
-
-            </div>
-          </v-card-item>
-        </v-card>
-      </v-app-bar>
-
-      <v-navigation-drawer
-          permanent
-          :rail="rail"
-          @click="rail = false"
-          elevation="0"
+  <v-layout full-height style="min-height: 100vh">
+    <v-app-bar elevation="0">
+      <v-card
+          class="bg-blue-darken-2"
+          variant="text"
+          elevation="1"
+          :rounded="0"
+          style="min-width: 100%;"
       >
+        <v-card-item>
+          <div class="d-flex justify-start align-center ga-4">
 
-        <v-list variant="text" density="default" nav rounded="lg">
-          <v-list-item
-              prepend-avatar="/assets/images/logotype.png"
-              rounded="lg"
-              title="GK Breeze"
-              subtitle="Inspector service"
-          >
-            <template #append>
-              <v-btn
-                  icon="mdi-menu-open"
-                  density="comfortable"
-                  color="blue-darken-1"
-                  variant="text"
-                  rounded="lg"
-                  :class="rail ? 'd-none' : ''"
-                  @click.stop="changeRail(!rail)"
-              >
-                <v-icon/>
-                <v-tooltip activator="parent">Скрыть меню</v-tooltip>
-              </v-btn>
-            </template>
-          </v-list-item>
-
-          <v-divider class="mb-1 mt-2"/>
-
-          <v-list-item
-              v-for="item of navItems"
-              :key="item._id"
-              :to="item.route"
-              :prepend-icon="item.prependIcon"
-              :active="item._id === activeItem"
-              :value="item.value"
-              :title="item.title"
-              color="blue-darken-1"
-              density="compact"
-              rounded="lg"
-              @click="activeItem = item._id"
-          />
-
-        </v-list>
-
-        <template #append>
-          <v-list variant="text" density="compact" rounded="lg" nav>
             <v-list-item
-                class="bg-grey-lighten-4"
-                prepend-icon="mdi-logout"
-                variant="text"
-                title="Выход"
-                @click="logout"
+                prepend-icon="mdi-account-cog-outline"
+                variant="tonal"
+                slim
+                nav
+                :title="profile.fullName"
+                :subtitle="profile.email"
+                @click="navigateToProfile"
             >
-              <v-tooltip activator="parent">Выйти из системы</v-tooltip>
+              <v-tooltip activator="parent" location="bottom">
+                Настройки профиля
+              </v-tooltip>
             </v-list-item>
-          </v-list>
-        </template>
-      </v-navigation-drawer>
 
-      <v-main>
-        <slot/>
-      </v-main>
+            <v-list-item
+                prepend-icon="mdi-bell"
+                title="Оповещения"
+                variant="tonal"
+                slim
+                nav
+                disabled
+                @click=""
+            >
+              <v-tooltip activator="parent" location="bottom">
+                Проверить оповещения
+              </v-tooltip>
+            </v-list-item>
 
-    </v-layout>
-  </v-card>
+            <v-list-item
+                prepend-icon="mdi-finance"
+                title="Статистика"
+                variant="tonal"
+                slim
+                nav
+                disabled
+                @click=""
+            >
+              <v-tooltip activator="parent" location="bottom">
+                Просмотр статистики
+              </v-tooltip>
+            </v-list-item>
+
+            <v-spacer/>
+
+            <v-label class="text-caption">
+              Текущий баланс аккаунта 1 000 объектов
+            </v-label>
+
+            <v-list-item
+                prepend-icon=""
+                variant="outlined"
+                class="white"
+                density="compact"
+                title="Пополнить баланс"
+                slim
+                disabled
+                nav
+                @click=""
+            >
+            </v-list-item>
+
+          </div>
+        </v-card-item>
+      </v-card>
+    </v-app-bar>
+
+    <v-navigation-drawer
+        permanent
+        :rail="rail"
+        @click="rail = false"
+        elevation="0"
+    >
+
+      <v-list variant="text" density="default" nav rounded="lg">
+        <v-list-item
+            prepend-avatar="/assets/images/logotype.png"
+            rounded="lg"
+            title="GK Breeze"
+            subtitle="Inspector service"
+        >
+          <template #append>
+            <v-btn
+                icon="mdi-menu-open"
+                density="comfortable"
+                color="blue-darken-3"
+                variant="text"
+                rounded="lg"
+                :class="rail ? 'd-none' : ''"
+                @click.stop="changeRail(!rail)"
+            >
+              <v-icon/>
+              <v-tooltip activator="parent">Скрыть меню</v-tooltip>
+            </v-btn>
+          </template>
+        </v-list-item>
+
+        <v-divider class="mb-1 mt-2"/>
+
+        <v-list-item
+            v-for="item of navItems"
+            :key="item._id"
+            :to="item.route"
+            :prepend-icon="item.prependIcon"
+            :active="item._id === activeItem"
+            :value="item.value"
+            :title="item.title"
+            color="blue-darken-1"
+            density="compact"
+            rounded="lg"
+            @click="activeItem = item._id"
+        />
+
+      </v-list>
+
+      <template #append>
+        <v-list variant="text" density="compact" rounded="lg" nav>
+          <v-list-item
+              class="bg-grey-lighten-4"
+              prepend-icon="mdi-logout"
+              variant="text"
+              title="Выход"
+              @click="logout"
+          >
+            <v-tooltip activator="parent">Выйти из системы</v-tooltip>
+          </v-list-item>
+        </v-list>
+      </template>
+    </v-navigation-drawer>
+
+    <v-main>
+      <slot/>
+    </v-main>
+
+  </v-layout>
 </template>
 
 <script>
